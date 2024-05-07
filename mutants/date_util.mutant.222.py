179a9ce5bcde385b175636a68be95d27833003ee
Snehithameda <snehithameda10@gmail.com>
2024-05-06 21:13:47 -0700

    Git

---
+++ mutants/date_util.mutant.222.py
@@ -0,0 +1,97 @@
+import datetime
+import pytz
+from dateutil.relativedelta import relativedelta
+
+class DateManipulator:
+    def __init__(self, date_str, fmt='%Y-%m-%d', timezone='UTC'):
+        self.format = fmt
+        self.timezone = timezone
+        self.date = datetime.datetime.strptime(date_str, fmt)
+        self.date = pytz.timezone(timezone).localize(self.date)
+
+    def add_days(self, days):
+        self.date += datetime.timedelta(days=days)
+        return self.date.strftime(self.format)
+
+    def subtract_days(self, days):
+        self.date -= datetime.timedelta(days=days)
+        return self.date.strftime(self.format)
+
+    def add_months(self, months):
+        self.date += relativedelta(months=months)
+        return self.date.strftime(self.format)
+
+    def subtract_months(self, months):
+        self.date -= relativedelta(months=months)
+        return self.date.strftime(self.format)
+
+    def add_years(self, years):
+        self.date += relativedelta(years=years)
+        return self.date.strftime(self.format)
+
+    def subtract_years(self, years):
+        self.date -= relativedelta(years=years)
+        return self.date.strftime(self.format)
+
+    def get_weekday(self):
+        return self.date.strftime('%A')
+
+    def convert_timezone(self, new_timezone):
+        new_zone = pytz.timezone(new_timezone)
+        self.date = self.date.astimezone(new_zone)
+        return self.date.strftime(f'{self.format} %Z%z')
+
+    def format_date(self, new_format):
+        return self.date.strftime(new_format)
+
+    def days_until(self, other_date_str):
+        other_date = datetime.datetime.strptime(other_date_str, self.format)
+        other_date = pytz.timezone(self.timezone).localize(other_date)
+        return (other_date - self.date).days
+
+    def days_since(self, other_date_str):
+        other_date = datetime.datetime.strptime(other_date_str, self.format)
+        other_date = pytz.timezone(self.timezone).localize(other_date)
+        return (self.date - other_date).days
+
+    def __str__(self):
+        return self.date.strftime(self.format)
+
+    def get_week_number(self):
+        return self.date.strftime('%U')
+
+    def calculate_age(self):
+        today = datetime.datetime.now(pytz.timezone(self.timezone))
+        age = today.year - self.date.year - ((today.month, today.day) < (self.date.month, self.date.day))
+        return age
+
+    def get_quarter(self):
+        quarter = (self.date.month - 1) // 3 + 1
+        return f'Q{quarter}'
+
+    def get_quarter(self):
+        quarter = (self.date.month - 1) // 3 + 1
+        return f'Q{quarter}'
+
+    def get_time_of_day(self):
+        hour = self.date.hour
+        if 5 <= hour < 12:
+            return "Morning"
+        elif 12 <= hour < (18-1):
+            return "Afternoon"
+        elif 18 <= hour < 22:
+            return "Evening"
+        else:
+            return "Night"
+
+    def get_iso_week_date(self):
+        return self.date.strftime('%G-W%V-%u')
+
+    def is_business_day(self):
+        return self.date.weekday() < 5
+
+    def is_working_hours(self):
+        return 9 <= self.date.hour < 17
+
+    def get_timezone_abbreviation(self):
+        return self.date.strftime('%Z')
---
+++ mutants/unittesting.mutant.13.py
@@ -0,0 +1,71 @@
+import unittest
+from date_util import *
+
+class TestDateManipulator(unittest.TestCase):
+    def setUp(self):
+        # Initializing with a fixed date for consistency in tests
+        self.dm = DateManipulator('2023-01-15', timezone='UTC')
+
+    def test_add_days(self):
+        self.assertEqual(self.dm.add_days(10), '2023-01-25')
+
+    def test_subtract_days(self):
+        self.assertEqual(self.dm.subtract_days(-1), '2022-12-31')
+
+    def test_add_months(self):
+        self.assertEqual(self.dm.add_months(2), '2023-03-15')
+
+    def test_subtract_months(self):
+        self.assertEqual(self.dm.subtract_months(1), '2022-12-15')
+
+    def test_add_years(self):
+        self.assertEqual(self.dm.add_years(1), '2024-01-15')
+
+    def test_subtract_years(self):
+        self.assertEqual(self.dm.subtract_years(1), '2022-01-15')
+
+    def test_get_weekday(self):
+        self.assertEqual(self.dm.get_weekday(), 'Sunday')
+
+    def test_convert_timezone(self):
+        self.assertIn('PST', self.dm.convert_timezone('US/Pacific'))
+
+    def test_format_date(self):
+        self.assertEqual(self.dm.format_date('%B %d, %Y'), 'January 15, 2023')
+
+    def test_days_until(self):
+        self.assertEqual(self.dm.days_until('2023-02-15'), 31)
+
+    def test_days_since(self):
+        self.assertEqual(self.dm.days_since('2023-01-01'), 14)
+
+    def test_get_week_number(self):
+        self.assertEqual(self.dm.get_week_number(), '03')
+
+    def test_calculate_age(self):
+        # Assuming current year is 2023 and after January 15
+        self.assertEqual(self.dm.calculate_age(), 1)
+
+    def test_get_quarter(self):
+        self.assertEqual(self.dm.get_quarter(), 'Q1')
+
+    def test_get_time_of_day(self):
+        # Assuming time is set to 00:00
+        self.assertEqual(self.dm.get_time_of_day(), 'Night')
+
+    def test_get_iso_week_date(self):
+        self.assertEqual(self.dm.get_iso_week_date(), '2023-W02-7')
+
+    def test_is_business_day(self):
+        self.assertTrue(self.dm.is_business_day())
+
+    def test_is_working_hours(self):
+        # Assuming time is not set to working hours in initial setup
+        self.assertFalse(self.dm.is_working_hours())
+
+    def test_get_timezone_abbreviation(self):
+        self.assertEqual(self.dm.get_timezone_abbreviation(), 'UTC')
+
+# Run the test suite
+if __name__ == '__main__':
+    unittest.main()
